9f32fde869659805f34d8933a451cd9669d8a3c1
Chris Walter <walt50@gmail.com>
2020-05-26 14:57:53 -0400

    MVP

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -7,11 +7,134 @@
 //
 
 import SwiftUI
+import LocalAuthentication
+import MapKit
+
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+
+    // faceID
+    @State private var isUnlocked = false
+    
+    @State private var locations = [CodableMKPointAnnotation]()
+    @State private var centerCoordinate = CLLocationCoordinate2D()
+    
+    @State private var selectedPlace: MKPointAnnotation?
+    @State private var showingPlaceDetails = false
+    
+    // for editView Alert
+    @State private var showingEditScreen = false
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
     }
+    
+    func loadData() {
+        let filename = getDocumentsDirectory().appendingPathComponent("SavedPlaces")
+
+        do {
+            let data = try Data(contentsOf: filename)
+            locations = try JSONDecoder().decode([CodableMKPointAnnotation].self, from: data)
+        } catch {
+            print("Unable to load saved data.")
+        }
+    }
+    
+    func saveData() {
+        do {
+            let filename = getDocumentsDirectory().appendingPathComponent("SavedPlaces")
+            let data = try JSONEncoder().encode(self.locations)
+            try data.write(to: filename, options: [.atomicWrite, .completeFileProtection])
+        } catch {
+            print("Unable to save data.")
+        }
+    }
+    
+    // for FaceID
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Please authenticate yourself to unlock your places."
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+
+                DispatchQueue.main.async {
+                    if success {
+                        self.isUnlocked = true
+                    } else {
+                        // error
+                    }
+                }
+            }
+        } else {
+            // no biometrics
+        }
+    }
+    
+    var body: some View {
+        ZStack {
+            if isUnlocked {
+                MapView(centerCoordinate: $centerCoordinate, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails, annotations: locations)
+                    .edgesIgnoringSafeArea(.all)
+                Circle()
+                    .fill(Color.blue)
+                    .opacity(0.3)
+                    .frame(width: 32, height: 32)
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            // create a new location
+                            let newLocation = CodableMKPointAnnotation()
+                            newLocation.title = "Example location"
+                            newLocation.coordinate = self.centerCoordinate
+                            self.locations.append(newLocation)
+                            
+                            self.selectedPlace = newLocation
+                            self.showingEditScreen = true
+                        }) {
+                            Image(systemName: "plus")
+                        }
+                        .padding()
+                        .background(Color.black.opacity(0.75))
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .clipShape(Circle())
+                        .padding(.trailing)
+                    }
+                    
+                }
+            } else {
+                   Button("Unlock Places") {
+                       self.authenticate()
+                   }
+                   .padding()
+                   .background(Color.blue)
+                   .foregroundColor(.white)
+                   .clipShape(Capsule())
+            }
+        }
+        .onAppear(perform: loadData)
+        .alert(isPresented: $showingPlaceDetails) {
+            Alert(title: Text(selectedPlace?.title ?? "Unknown"), message: Text(selectedPlace?.subtitle ?? "Missing place information."), primaryButton: .default(Text("OK")), secondaryButton: .default(Text("Edit")) {
+                // edit this place
+                self.showingEditScreen = true
+            })
+        }
+        .sheet(isPresented: $showingEditScreen, onDismiss: saveData) {
+            if self.selectedPlace != nil { // can't use if let, so we do this instead.
+                EditView(placemark: self.selectedPlace!)
+            }
+        }
+            
+    }
+   
+    
+   
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ BucketList/Result.swift
@@ -0,0 +1,32 @@
+//
+//  Result.swift
+//  BucketList
+//
+//  Created by Christopher Walter on 5/24/20.
+//  Copyright © 2020 Christopher Walter. All rights reserved.
+//
+
+import Foundation
+
+struct Result: Codable {
+    let query: Query
+}
+
+struct Query: Codable {
+    let pages: [Int: Page]
+}
+
+struct Page: Codable, Comparable {
+    let pageid: Int
+    let title: String
+    let terms: [String: [String]]?
+
+    // added for comparable
+    static func < (lhs: Page, rhs: Page) -> Bool {
+        lhs.title < rhs.title
+    }
+    
+    var description: String {
+        terms?["description"]?.first ?? "No further information"
+    }
+}
